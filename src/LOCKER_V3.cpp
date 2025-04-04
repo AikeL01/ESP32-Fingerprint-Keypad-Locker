c86590b5cec9e232a2ebc4ba8e9baa90537e2cdf
Aikel <79527447+AikeL01@users.noreply.github.com>
2025-04-04 09:51:43 +0800

    Update LOCKER_V3.cpp

--- src/LOCKER_V3.cpp
+++ src/LOCKER_V3.cpp
@@ -13,6 +13,7 @@
 #define I2C_ADDR 0x27      // I2C address of LCD
 #define LCD_COLS 16
 #define LCD_ROWS 2
+#define WAKEUP_PIN 14
 
 // Create hardware objects
 LCD_I2C lcd(I2C_ADDR, LCD_COLS, LCD_ROWS);
@@ -57,9 +58,6 @@ const int BUZZER_LONG_BEEP = 200;
 // Add a counter for consecutive '*' presses
 int star_press_count = 0;
 const int STAR_PRESS_THRESHOLD = 12;
-
-// Define the GPIO pin for wake-up
-#define WAKEUP_PIN 14
 
 // Function declarations
 void showReadyScreen();
