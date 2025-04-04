e879871642249c2f398b21f326ddb3ba3bc00597
Aikel <79527447+AikeL01@users.noreply.github.com>
2025-04-04 09:55:04 +0800

    Update LOCKER_V3.cpp

--- src/LOCKER_V3.cpp
+++ src/LOCKER_V3.cpp
@@ -13,7 +13,6 @@
 #define I2C_ADDR 0x27      // I2C address of LCD
 #define LCD_COLS 16
 #define LCD_ROWS 2
-#define WAKEUP_PIN 14
 
 // Create hardware objects
 LCD_I2C lcd(I2C_ADDR, LCD_COLS, LCD_ROWS);
@@ -33,7 +32,7 @@ byte colPins[COLS] = { 18, 19, 23 };
 SimpleKeypad keypad(keys, rowPins, colPins, ROWS, COLS);
 
 // Password variables
-const String DEFAULT_PASSWORD = "0000";
+const String DEFAULT_PASSWORD = "000000";
 String input_password = "";
 int wrong_attempts = 0;
 const int MAX_WRONG_ATTEMPTS = 5;
@@ -102,7 +101,6 @@ void setup() {
     setPassword(DEFAULT_PASSWORD);
   }
 
-  pinMode(WAKEUP_PIN, INPUT);  // Set wake-up pin as input
   setupHibernation();
 
   showReadyScreen();
